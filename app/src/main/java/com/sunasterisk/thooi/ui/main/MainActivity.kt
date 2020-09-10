686d9bc57ca194c62ddb2fdd1708189947056519
congvc <nogogold1998>
2020-09-10 14:17:11 +0700

    Write and setup navigation graph

--- app/src/main/java/com/sunasterisk/thooi/ui/main/MainActivity.kt
+++ app/src/main/java/com/sunasterisk/thooi/ui/main/MainActivity.kt
@@ -2,14 +2,23 @@ package com.sunasterisk.thooi.ui.main
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.sunasterisk.thooi.R
 import com.sunasterisk.thooi.base.BaseActivity
 import com.sunasterisk.thooi.databinding.ActivityMainBinding
 
 class MainActivity : BaseActivity<ActivityMainBinding>() {
 
+    private val navController by lazy { findNavController(R.id.navHostFragment) }
+
     override fun onCreateBinding(
         inflater: LayoutInflater,
         container: ViewGroup?,
-        attachToRoot: Boolean
+        attachToRoot: Boolean,
     ) = ActivityMainBinding.inflate(inflater)
+
+    override fun setupViews() {
+        binding.bottomNavView.setupWithNavController(navController)
+    }
 }
